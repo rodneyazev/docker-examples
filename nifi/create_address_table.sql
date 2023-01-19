a36c92beb03ef31bb7fd7f755165fa9767359174
rodneyazev <rodney.n.azevedo@gmail.com>
2023-01-19 16:57:30 -0300

    DOCKER EXAMPLES

---
+++ nifi/create_address_table.sql
@@ -0,0 +1,10 @@
+create table address (
+	id INT,
+	location_id INT,
+	address_1 VARCHAR(100),
+	address_2 VARCHAR(100),
+	city VARCHAR(100),
+	state_province VARCHAR(10),
+	postal_code INT,
+	country VARCHAR(10)	
+)
